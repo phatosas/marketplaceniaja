97628565d632759f77062a3d85a764473cf67df2
phatosas <phatosas@yahoo.com>
2016-06-14 06:03:31 -0400

    edited route file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,24 +11,22 @@
 |
 */
 
-
 Route::get('/', function () {
-		$products = \marketplaceniaja\Product::with('type')->orderBy('id', 'desc')->get();
-		$types = \marketplaceniaja\Type::with('products')->orderBy('id', 'desc')->get();
-		return view('welcome')->with('products',$products)->with('types',$types);
+	$products = \marketplaceniaja\Product::with('type')->orderBy('id', 'desc')->get();
+	$types = \marketplaceniaja\Type::with('products')->orderBy('id', 'desc')->get();
+    return view('welcome')->with('products',$products)->with('types',$types);
 });
 
-Route::group(['middleware' => ['web']], function () {
-	Route::get('/products', 'ProductController@getIndex');
-	Route::get('/products/show/{name?}', 'ProductController@getShow');
-	Route::get('/products/create', 'ProductController@getCreate');
-	Route::post('/products/create', 'ProductController@postCreate');
-	Route::get('/products/edit/{id?}', 'ProductController@getEdit');
-	Route::post('/products/edit/', 'ProductController@postEdit');
-	Route::get('/products/delete/{title?}', 'ProductController@getDelete');
-	Route::get('/states', 'StateController@getIndex');
-	Route::get('/states/show/{id?}', 'StateController@getShow');
 
+Route::get('/products', 'ProductController@getIndex');
+Route::get('/products/show/{name?}', 'ProductController@getShow');
+Route::get('/products/create', 'ProductController@getCreate');
+Route::post('/products/create', 'ProductController@postCreate');
+Route::get('/products/edit/{id?}', 'ProductController@getEdit');
+Route::post('/products/edit/', 'ProductController@postEdit');
+Route::get('/products/delete/{title?}', 'ProductController@getDelete');
+Route::get('/states', 'StateController@getIndex');
+Route::get('/states/show/{id?}', 'StateController@getShow');
 
 	#--------------------------------------
 	# Authentication
@@ -41,63 +39,6 @@ Route::group(['middleware' => ['web']], function () {
 	Route::post('/register', 'Auth\AuthController@postRegister');
 
 	Route::get('/logout', 'Auth\AuthController@logout');
+	
 
-	Route::get('/show-login-status', function() {
-
-	# You may access the authenticated user via the Auth facade
-	$user = Auth::user();
-
-	if($user) {
-		echo 'You are logged in.';
-		dump($user->toArray());
-	} else {
-		echo 'You are not logged in.';
-	}
-
-	return;
-	});
-
-	Route::get('/practice', function() {
-		$random = new Random();
-		return $random->getRandomString(10);
-	});
-
-
-
-	Route::get('/debug', function() {
-
-		echo '<pre>';
-
-		echo '<h1>Environment</h1>';
-		echo App::environment().'</h1>';
-
-		echo '<h1>Debugging?</h1>';
-		if(config('app.debug')) echo "Yes"; else echo "No";
-
-		echo '<h1>Database Config</h1>';
-		/*
-		The following line will output your MySQL credentials.
-		Uncomment it only if you're having a hard time connecting to the database and you
-		need to confirm your credentials.
-		When you're done debugging, comment it back out so you don't accidentally leave it
-		running on your live server, making your credentials public.
-		*/
-		//print_r(config('database.connections.mysql'));
-
-		echo '<h1>Test Database Connection</h1>';
-		try {
-			$results = DB::select('SHOW DATABASES;');
-			echo '<strong style="background-color:green; padding:5px;">Connection confirmed</strong>';
-			echo "<br><br>Your Databases:<br><br>";
-			print_r($results);
-		}
-		catch (Exception $e) {
-			echo '<strong style="background-color:crimson; padding:5px;">Caught exception: ', $e->getMessage(), "</strong>\n";
-		}
-
-		echo '</pre>';
-
-	});
-
-});
 
--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -1,17 +1,21 @@
 <?php
+
 namespace marketplaceniaja\Providers;
+
 use Illuminate\Routing\Router;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+
 class RouteServiceProvider extends ServiceProvider
 {
     /**
-     * This namespace is applied to the controller routes in your routes file.
+     * This namespace is applied to your controller routes.
      *
      * In addition, it is set as the URL generator's root namespace.
      *
      * @var string
      */
     protected $namespace = 'marketplaceniaja\Http\Controllers';
+
     /**
      * Define your route model bindings, pattern filters, etc.
      *
@@ -21,8 +25,10 @@ class RouteServiceProvider extends ServiceProvider
     public function boot(Router $router)
     {
         //
+
         parent::boot($router);
     }
+
     /**
      * Define the routes for the application.
      *
@@ -31,7 +37,22 @@ class RouteServiceProvider extends ServiceProvider
      */
     public function map(Router $router)
     {
-        $router->group(['namespace' => $this->namespace], function ($router) {
+        $this->mapWebRoutes($router);
+
+        //
+    }
+
+    /**
+     * Define the "web" routes for the application.
+     *
+     * @param  \Illuminate\Routing\Router  $router
+     * @return void
+     */
+    protected function mapWebRoutes(Router $router)
+    {
+        $router->group([
+            'namespace' => $this->namespace, 'middleware' => 'web'
+        ], function ($router) {
             require app_path('Http/routes.php');
         });
     }
